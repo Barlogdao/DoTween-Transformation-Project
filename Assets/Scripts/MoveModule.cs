aea8102c27e71d234e42f001c8b60896f13361c6
Barlogdao <109853370+Barlogdao@users.noreply.github.com>
2024-03-06 17:00:39 +0300

    Combine Modules

--- Assets/Scripts/MoveModule.cs
+++ Assets/Scripts/MoveModule.cs
@@ -3,7 +3,8 @@ using DG.Tweening;
 
 public class MoveModule : MonoBehaviour
 {
-    [SerializeField] private Transform _target;
+    [SerializeField] private Vector3 _positionOffset;
+
     [SerializeField] private float _speed;
     [SerializeField] LoopType _loopType = LoopType.Yoyo;
     [SerializeField] Ease _ease = Ease.Linear;
@@ -15,6 +16,14 @@ public class MoveModule : MonoBehaviour
 
     private void Move()
     {
-        transform.DOMove(_target.transform.position, _speed).SetSpeedBased().SetLoops(-1,_loopType).SetEase(_ease);
+        Vector3 targetPosition = transform.position + _positionOffset;
+        transform.DOMove(targetPosition, _speed).SetSpeedBased().SetLoops(-1,_loopType).SetEase(_ease);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+
+        Gizmos.DrawSphere(transform.position + _positionOffset, 0.5f);
     }
 }
